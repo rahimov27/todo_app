eb5c4032d010a0c4f961dea8b1519ba963d4bf7c
rahimov27 <89564054+rahimov27@users.noreply.github.com>
2025-03-27 10:01:39 +0100

    added bottom na bar

--- lib/view/home_screen.dart
+++ lib/view/home_screen.dart
@@ -5,101 +5,106 @@ import 'package:todo_app/view/home_widgets/home_search.dart';
 import 'package:todo_app/view/home_widgets/home_todo_card.dart';
 import 'package:todo_app/view/home_widgets/home_welcome_text.dart';
 
-class TodoView extends StatelessWidget {
-  const TodoView({super.key});
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea(
-        child: Padding(
-          padding: const EdgeInsets.only(left: 18, right: 18, top: 20),
-          child: SingleChildScrollView(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                // todos part
-                HomeWelcomeText(),
-                SizedBox(height: 14),
-                HomeSearch(),
-                SizedBox(height: 20),
-                HomeTodoCard(),
-                SizedBox(height: 14),
-                HomeTodoCard(percentage: 10),
+      appBar: AppBar(
+        surfaceTintColor: Colors.transparent,
+        toolbarOpacity: 0,
+        automaticallyImplyLeading: false,
+        backgroundColor: AppColors.scaffoldColor,
+        toolbarHeight: 0,
+      ),
+      body: Padding(
+        padding: const EdgeInsets.only(left: 18, right: 18, top: 20),
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              // todos part
+              HomeWelcomeText(),
+              SizedBox(height: 14),
+              HomeSearch(),
+              SizedBox(height: 20),
+              HomeTodoCard(),
+              SizedBox(height: 14),
+              HomeTodoCard(percentage: 10),
 
-                // folders part
-                SizedBox(height: 24),
-                Text(
-                  "Папки",
-                  style: TextStyle(
-                    letterSpacing: -0.7,
-                    fontFamily: "Inter-Bold",
-                    fontSize: 28,
-                    color: Colors.white,
+              // folders part
+              SizedBox(height: 24),
+              Text(
+                "Папки",
+                style: TextStyle(
+                  letterSpacing: -0.7,
+                  fontFamily: "Inter-Bold",
+                  fontSize: 28,
+                  color: Colors.white,
+                ),
+              ),
+              SizedBox(height: 14),
+              Row(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Column(
+                    children: [
+                      FolderCard(
+                        width: MediaQuery.of(context).size.width / 2.10,
+                        height: MediaQuery.of(context).size.width / 1.3,
+                        title: "Задачи по работе и заводу",
+                        subtitle: "Заполнение всех документов на заводе!",
+                        progress: 30,
+                        color: AppColors.appPurple,
+                      ),
+                      SizedBox(height: 10),
+                      FolderCard(
+                        width: MediaQuery.of(context).size.width / 2.10,
+                        height: MediaQuery.of(context).size.width / 2.5,
+                        title: "Задачи по дому",
+                        subtitle: "",
+                        progress: 60,
+                        color: AppColors.pink,
+                      ),
+                    ],
                   ),
-                ),
-                SizedBox(height: 14),
-                Row(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Column(
-                      children: [
-                        FolderCard(
-                          width: MediaQuery.of(context).size.width / 2.10,
-                          height: MediaQuery.of(context).size.width / 1.3,
-                          title: "Задачи по работе и заводу",
-                          subtitle: "Заполнение всех документов на заводе!",
-                          progress: 30,
-                          color: AppColors.appPurple,
-                        ),
-                        SizedBox(height: 10),
-                        FolderCard(
-                          width: MediaQuery.of(context).size.width / 2.10,
-                          height: MediaQuery.of(context).size.width / 2.5,
-                          title: "Задачи по дому",
-                          subtitle: "",
-                          progress: 60,
-                          color: AppColors.pink,
-                        ),
-                      ],
-                    ),
 
-                    Spacer(),
+                  Spacer(),
 
-                    Column(
-                      children: [
-                        FolderCard(
-                          width: MediaQuery.of(context).size.width / 2.50,
-                          height: MediaQuery.of(context).size.width / 1.7,
-                          title: "Задачи по курсам",
-                          subtitle: "Заполнение всех документов на заводе!",
-                          progress: 20,
-                          color: AppColors.cardBlue,
-                        ),
-                        SizedBox(height: 10),
-                        FolderCard(
-                          width: MediaQuery.of(context).size.width / 2.50,
-                          height: MediaQuery.of(context).size.width / 1.7,
-                          title: "Задачи по учебе",
-                          subtitle: "Заполнение всех документов на заводе!",
-                          progress: 80,
-                          color: AppColors.appGreen,
-                        ),
-                      ],
-                    ),
-                  ],
-                ),
-                SizedBox(height: 10),
-                FolderCard(
-                  width: double.infinity,
-                  height: MediaQuery.of(context).size.width / 2.5,
-                  title: "Задачи по курсам",
-                  subtitle: "",
-                  progress: 20,
-                  color: AppColors.cardYellow,
-                ),
-              ],
-            ),
+                  Column(
+                    children: [
+                      FolderCard(
+                        width: MediaQuery.of(context).size.width / 2.50,
+                        height: MediaQuery.of(context).size.width / 1.7,
+                        title: "Задачи по курсам",
+                        subtitle: "Заполнение всех документов на заводе!",
+                        progress: 20,
+                        color: AppColors.cardBlue,
+                      ),
+                      SizedBox(height: 10),
+                      FolderCard(
+                        width: MediaQuery.of(context).size.width / 2.50,
+                        height: MediaQuery.of(context).size.width / 1.7,
+                        title: "Задачи по учебе",
+                        subtitle: "Заполнение всех документов на заводе!",
+                        progress: 80,
+                        color: AppColors.appGreen,
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+              SizedBox(height: 10),
+              FolderCard(
+                width: double.infinity,
+                height: MediaQuery.of(context).size.width / 2.5,
+                title: "Задачи по курсам",
+                subtitle: "",
+                progress: 20,
+                color: AppColors.cardYellow,
+              ),
+            ],
           ),
         ),
       ),
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,12 +8,12 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:todo_app/view/todo_view.dart';
+import 'package:todo_app/view/home_screen.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const TodoView());
+    await tester.pumpWidget(const HomeScreen());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
