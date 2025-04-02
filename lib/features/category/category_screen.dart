030a8e8840c2d3d7fc2e2c7c4a260e4297c3f9fa
rahimov27 <89564054+rahimov27@users.noreply.github.com>
2025-04-03 00:11:28 +0200

    added category screen with tasks

--- lib/features/category/category_screen.dart
+++ lib/features/category/category_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:todo_app/features/home/widgets/home_search.dart';
 import 'package:todo_app/features/home/widgets/home_todo_card.dart';
 import 'package:todo_app/features/home/viewmodel/todo_viewmodel.dart';
 
@@ -9,24 +10,58 @@ class CategoryScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final todoProvider = Provider.of<TodoViewmodel>(context);
-    print(todoProvider.todos.length);
     return Scaffold(
-      body: Padding(
-        padding: const EdgeInsets.all(10),
-        child: ListView.builder(
-          shrinkWrap: true,
-          itemCount: todoProvider.todos.length,
-          itemBuilder: (context, index) {
-            return HomeTodoCard(
-              title: todoProvider.todos[index].title,
-              subtitle: todoProvider.todos[index].subtitle ?? "Нету",
-              time: todoProvider.todos[index].time,
-              date: todoProvider.todos[index].date,
-              progress: todoProvider.todos[index].progress,
-            );
-          },
-        ),
-      ),
+      appBar: AppBar(),
+      body:
+          todoProvider.todos.isNotEmpty
+              ? Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 18),
+                child: Column(
+                  children: [
+                    SizedBox(height: 24),
+                    HomeSearch(),
+                    SizedBox(height: 24),
+                    ListView.builder(
+                      shrinkWrap: true,
+                      itemCount: todoProvider.todos.length,
+                      itemBuilder: (context, index) {
+                        return HomeTodoCard(
+                          title: todoProvider.todos[index].title,
+                          subtitle:
+                              todoProvider.todos[index].subtitle ?? "Нету",
+                          time: todoProvider.todos[index].time,
+                          date: todoProvider.todos[index].date,
+                          progress: todoProvider.todos[index].progress,
+                        );
+                      },
+                    ),
+                  ],
+                ),
+              )
+              : Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 18),
+                child: Column(
+                  children: [
+                    SizedBox(height: 24),
+                    HomeSearch(),
+                    SizedBox(height: 24),
+                    HomeTodoCard(
+                      title: "Добавляйте свои задачи",
+                      subtitle: "Пишите свои описания",
+                      time: DateTime.now(),
+                      date: DateTime.now(),
+                      progress: 35,
+                    ),
+                    HomeTodoCard(
+                      title: "Добавляйте свои задачи",
+                      subtitle: "Пишите свои описания",
+                      time: DateTime.now(),
+                      date: DateTime.now(),
+                      progress: 80,
+                    ),
+                  ],
+                ),
+              ),
     );
   }
 }
